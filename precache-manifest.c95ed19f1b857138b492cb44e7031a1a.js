1eaab5a11b0f554e07213715c066633a8a2ac1f5
Vanilla <solmarians05@gmail.com>
2020-06-25 12:04:55 +0300

    Updates

---
+++ precache-manifest.c95ed19f1b857138b492cb44e7031a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ccc70d0c3b9a170805d1e4771a9a14",
+    "url": "/learn/index.html"
+  },
+  {
+    "revision": "25f75070182c385247c7",
+    "url": "/learn/static/css/main.6d3e28cd.chunk.css"
+  },
+  {
+    "revision": "f407830e7f971fd49cac",
+    "url": "/learn/static/js/2.66b0b304.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/learn/static/js/2.66b0b304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f75070182c385247c7",
+    "url": "/learn/static/js/main.ff9272da.chunk.js"
+  },
+  {
+    "revision": "790ec935dc7a51994e47",
+    "url": "/learn/static/js/runtime-main.0e15140b.js"
+  }
+]);
